8687e81d9d6a0a97a87a644082a2abcab471a99b
Lucas Rumney <lucasrumney94@gmail.com>
2016-03-26 15:37:08 -0400

    Added Blurring and Glasses Collectable Code

---
+++ SaffronSpectacle/Assets/Scripts/applyGlassesBlur.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class applyGlassesBlur : MonoBehaviour {
+
+    private UnityStandardAssets.ImageEffects.BlurOptimized optimizedBlur;
+    private int highestGlassesAchieved;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        optimizedBlur = this.gameObject.GetComponent<UnityStandardAssets.ImageEffects.BlurOptimized>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        highestGlassesAchieved = GameObject.FindGameObjectWithTag("Player").GetComponent<playerAttributes>().highestGlassesAchieved;
+        optimizedBlur.blurSize = 10 - (2 * (highestGlassesAchieved));
+        if (highestGlassesAchieved > 0)
+        {
+            optimizedBlur.blurIterations = 3;
+            optimizedBlur.downsample = 1;
+        }
+        if (highestGlassesAchieved > 2)
+        {
+            optimizedBlur.blurIterations = 2;
+            optimizedBlur.downsample = 0;
+        }
+        if (highestGlassesAchieved == 5)
+        {
+            optimizedBlur.enabled = false;
+        }
+	}
+}
--- SaffronSpectacle/Assets/Scripts/playerAttributes.cs
+++ SaffronSpectacle/Assets/Scripts/playerAttributes.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class playerAttributes : MonoBehaviour {
 
-    public int[] numberOfGlassPieces = {0,0,0,0,0};
+    public int[] numberOfGlassPieces = {0,0,0,0,0,0};
+    public int[] numberOfGlassPiecesReq = { 2, 2, 2, 2, 2, 2 };
+    public bool[] glassesAchieved = { false, false, false, false, false, false };
+    public int highestGlassesAchieved = 0;
 
 	// Use this for initialization
 	void Start ()
@@ -14,6 +17,19 @@ public class playerAttributes : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        //This Block calculates the highest achieved glasses
+        
+        for (int i = 0; i < numberOfGlassPieces.Length; i++)
+        {
+            if (numberOfGlassPieces[i] >= numberOfGlassPiecesReq[i])
+                glassesAchieved[i] = true;
+
+            if (glassesAchieved[i])
+                highestGlassesAchieved = i;
+        }
+
+
+
         //Debug.Log(numberOfGlassPieces[0] + "  " + numberOfGlassPieces[1] + "  " + numberOfGlassPieces[2] + "  " + numberOfGlassPieces[3] + "  " + numberOfGlassPieces[4]);
 	}
 }
