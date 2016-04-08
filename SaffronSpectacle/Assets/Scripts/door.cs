6e970795e76678aa20aa6bfa1fa74689769e6b8a
Lucas Rumney <lucasrumney94@gmail.com>
2016-04-08 18:21:11 -0400

    Fixed Door X axis flipping and adjusted door hingeJoint;

--- SaffronSpectacle/Assets/Scripts/door.cs
+++ SaffronSpectacle/Assets/Scripts/door.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class door : MonoBehaviour {
 
 
-    public Vector3 hinge = new Vector3(0.0f,1.0f,0.0f);
+    //public Vector3 hinge = new Vector3(0.0f,1.0f,0.0f);
     // Use this for initialization
     void Start ()
     {
@@ -19,11 +19,11 @@ public class door : MonoBehaviour {
 
     void interact()
     {
-        InvokeRepeating("doorOpen",.01f,.1f);
+        //InvokeRepeating("doorOpen",.01f,.1f);
     }
 
     void doorOpen()
     {
-        this.transform.RotateAround(hinge, 1.0f);
+        //this.transform.Rotate();
     }
 }
